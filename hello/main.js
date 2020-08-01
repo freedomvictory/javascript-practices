36e9bc30f38a3f37267c25ef8cacec840a54ee22
dingguoliang <dinggl9367@163.com>
2020-08-01 10:28:09 +0800

    init hello

--- hello/main.js
+++ hello/main.js
@@ -1,5 +1,19 @@
 
+/*
 const mod = "hello, this is a simple js file"
 
 
 alert(mod)
+
+
+alert(typeof(document.getElementById("purchase")))
+
+ */
+
+const items = document.getElementsByTagName("li")
+for (var i = 0; i < items.length; i++){
+    alert(typeof(items[i]))
+}
+
+
+
